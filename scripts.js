445f9611fcfbb5fe48339297d1842fae872f274c
TshepoMelesi <twmelesi@gmail.com>
2022-08-12 22:25:58 +0200

    new look for desktop

--- scripts.js
+++ scripts.js
@@ -1,42 +1,92 @@
-const generatorBtn = document.getElementById('generate')
-const saveBtn = document.getElementById('save')
-const emailBtn = document.getElementById('restore')
-const passwordLen = document.getElementById('passwordLength')
-const passwordTitle = document.getElementById('passwordTitle')
-const characters = '123#aZb?/YcX#d6(WeV?fU4)56#gThSi7*RjQ?kP#l7%89!O#mNnMo#8LpKq0/12J@rI9sHtGu&F345v!EwDxCy0BzA'
-let generatedPassword = ''
-let storage = []
-let passwordTag = document.getElementById('password')
+/*header code ---------------------------------------*/
+const header = document.getElementById('header')
+const headerElements = `
+<div class="container">
+        <div class="logo">
+            <h1>PAS Generator</h1>
+        </div>
+        
+        <div class="navigator">
+            <ul>
+                <li>
+                    <a href="./index.html">Home</a>
+                </li>
+                <li>
+                    <a href="./index.html">Notes</a>
+                </li>
+                <li>
+                    <a href="./index.html">Learn</a>
+                </li>
+                <li>
+                    <a href="./index.html">About</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+`
+/*---------------------------------------------------*/
 
-function generate(_characters, _length){
-    let _len = _length.value - 1
-    let char
-    generatedPassword = ''
-    for(let i=0; i<=_len; i++){
-        char = _characters[Math.ceil(Math.random() * _characters.length)]
-        generatedPassword += char
+/*---------------------------------------------------*/
+const generateBtn = document.getElementById('generate')
+const addNoteBtn = document.getElementById('addNote')
+const limit = document.getElementById('length')
+const displayPassword = document.getElementById('GeneratedPassword')
+const checkNum = document.getElementById('includeNumbers')
+const checkAlphas = document.getElementById('alphabets')
+const checkLow = document.getElementById('lowerAlpha')
+const checkChar = document.getElementById('specialChars')
+const numbers = '1234567890'
+const alphabets = 'eQzxWqwERrTYtyUuioIASpOPaDMbFGHsdfJKLZXghjkCVnmBNlcv'
+const character = '!@#$%^&*()_+={}[]|?/<,>.'
+/*---------------------------------------------------*/
+
+class Generate{
+    constructor(_inNum = '', _alphas = '', _inChar = ''){
+        this.numbers = _inNum
+        this.alphabets = _alphas
+        this.character = _inChar
+        this.holder = ''
+        this.elements = ''
+    }
+    generate(_limit){
+        this.holder += this.alphabets + this.numbers + this.character
+        for (let i = 0; i <= _limit; i++) {
+            if( !(i === 0) && i % 25 === 0 ) {
+                this.elements += '\n'
+            } else {
+                this.elements += this.holder[Math.floor(Math.random() * this.holder.length)]
+            }
+        }
+        return this.elements
     }
 }
 
-generatorBtn.addEventListener('click', ev =>{
-    generate(characters, passwordLen)
-    if (passwordLen.value >= 4) {
-        passwordTag.innerText = generatedPassword
-    } else {
-        passwordTag.innerText = 'please insert a minimum of 4 characters'}
+window.addEventListener('load', () => {
+    header.innerHTML = headerElements
+
+    generateBtn.addEventListener('click', generatePass)
 })
 
-saveBtn.addEventListener('click', ev => {
-    let entry = {'title': passwordTitle.value, 'pass': generatedPassword}
-    storage.push(entry)
-})
+function generatePass() {
+    const checked = [checkNum.checked, checkAlphas.checked, checkChar.checked]
 
-emailBtn.addEventListener('click', ev => {
-    // todo: add emailing functionality
-    console.clear()
-    if (storage.length > 0){
-        for (let i = 0; i<=storage.length -1; i++){
-        console.log(storage[i].title + ': ' + storage[i].pass)
+    checked.forEach((value => {
+        console.log(value)
+    }))
+
+
+    if( checkNum.checked && checkAlphas.checked && checkChar.checked ) {
+        displayPassword.innerText = ''
+        let password = new Generate(numbers, alphabets, character)
+        let newPass = password.generate(limit.value)
+        displayPassword.innerText = newPass
+        console.log(newPass)
     }
-    }
-})
+}
+
+
+
+
+
+
+
